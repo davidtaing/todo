f9e3251573f97cfaeb971752b593783e4e0f20fd
David Taing <adavidtaing@gmail.com>
2020-08-16 21:36:28 +1000

    Implemented strikethrough when completed prop is true

--- frontend/src/components/TodoListItem.js
+++ frontend/src/components/TodoListItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const TodoListItem = ({todoListItem}) => { 
+    const { text, completed } = todoListItem;
+
     return (
         <div className="listitem">
             <input type="checkbox" />
-            <h className="listitem-text">{todoListItem.text}</h>
+            <h className="listitem-text">
+                { completed ? <strike>{text}</strike> : text}
+            </h>
         </div>
     )
 };
