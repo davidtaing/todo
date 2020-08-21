cc1cae74ee1f39ced9e53d868e7fd35b4b690fb8
David Taing <adavidtaing@gmail.com>
2020-08-21 21:26:19 +1000

    Fixed bug where the empty AddTodo input box triggers action

---
+++ frontend/src/components/AddTodo.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const AddTodo = ({addTodo}) => {
+    const [text, setText] = useState('');
+    
+    return (
+        <div className="add-todo-container">
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                if (text) { 
+                    addTodo(text);
+                }
+            }}>
+                <input 
+                    name="todo-text"
+                    type="text"
+                    value={text}
+                    placeholder="Next todo" 
+                    onChange={(e) => setText(e.target.value)} />
+                <button 
+                    className="add-button"
+                    type="submit" >
+                    Add
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default AddTodo;
