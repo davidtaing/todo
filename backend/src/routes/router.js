7856fd43d0dd3e363b1dab277db4a7e6bdcb117f
David Taing <adavidtaing@gmail.com>
2020-08-25 23:25:38 +1000

    Made the todos GET route return an array of todos

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -3,7 +3,25 @@ const router = express.Router();
 
 // GET Todos
 router.get(('/todos'), (req, res, next) => {
-    res.send("Todos");
+    const result = [
+        {
+            id: "1",
+            title: "Finish this project",
+            completed: true,
+        },
+        {
+            id: "2",
+            title: "Learn something",
+            completed: true,
+        },        
+        {
+            id: "3",
+            title: "Become a programmer",
+            completed: false,
+        },
+    ];
+
+    res.status(200).json(result);
 });
 
 // POST Todo
