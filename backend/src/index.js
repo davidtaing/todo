8e5e243c402e6b79e4ffce9d358b96a97c15f0e8
David Taing <adavidtaing@gmail.com>
2020-08-25 23:26:09 +1000

    Added CORS to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/router';
 
 const PORT = 3001;
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/', router);
 
