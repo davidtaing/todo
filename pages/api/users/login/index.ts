fd5cc21cc6720edaef6115af452d8c8fbd1f87a1
David Taing <adavidtaing@gmail.com>
2021-10-02 22:16:34 +1000

    Feat: Login Route w/ Partial Implementation & Tests

--- pages/api/users/login/index.ts
+++ pages/api/users/login/index.ts
@@ -1,7 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { signInWithEmailAndPassword } from "@firebase/auth";
+
+// Local Firebase Auth Object
+import auth from "../../../../src/api/firebase/auth";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res
-    .status(501)
-    .json({message: "Not Implemented"});
+  const method = req?.method;
+
+  if (method !== "POST") {
+    res.status(405).json({ message: "Method Not Allowed" });
+  } else {
+    postHandler(req, res);
+  }
+}
+
+export function postHandler(req: NextApiRequest, res: NextApiResponse): void {
+  const { email, password } = req?.body;
+
+  if (!email || !password) {
+    res.status(400).json({ message: "Bad Request" });
+  } else {
+    try {
+      const userCredential = signInWithEmailAndPassword(auth, email, password);
+      res.status(501).json({ message: "Not Yet Implemented" });
+    } catch (err) {
+      res.status(501).json({ message: "Not Yet Implemented" });
+    }
+  }
 }
