d12adc74d62b175d5fce42b9d54ba2ce53763af3
David Taing <adavidtaing@gmail.com>
2021-10-03 01:44:14 +1000

    Fix: Add Missing Asnyc Declaration

--- pages/api/users/login/index.ts
+++ pages/api/users/login/index.ts
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 // Local Firebase Auth Object
-import { auth, signInWithEmailAndPassword } from "../../../../src/api/firebase/auth";
+import {
+  auth,
+  signInWithEmailAndPassword,
+} from "../../../../src/api/firebase/auth";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const method = req?.method;
@@ -13,17 +16,24 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   }
 }
 
-export function postHandler(req: NextApiRequest, res: NextApiResponse): void {
+export async function postHandler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   const { email, password } = req?.body;
 
   if (!email || !password) {
     res.status(400).json({ message: "Bad Request" });
-  } else {
-    try {
-      const userCredential = signInWithEmailAndPassword(auth, email, password);
-      res.status(303).json({ userCredential });
-    } catch (err) {
-      res.status(501).json({ message: "Not Yet Implemented" });
-    }
+  }
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    console.log(userCredential);
+    res.status(303).json({ userCredential });
+  } catch (err) {
+    console.error(err);
+    res.status(501).json({ message: "Not Yet Implemented" });
   }
 }
