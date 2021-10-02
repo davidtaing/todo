efa2ac2c6ccea1ff2c51bee0a522d705a4afd970
David Taing <adavidtaing@gmail.com>
2021-10-02 20:48:00 +1000

    Chore: Add semicolon for consistency

--- src/api/firebase/firebase.ts
+++ src/api/firebase/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-}
+};
 
 const firebaseApp : FirebaseApp = initializeApp(firebaseConfig);
 
