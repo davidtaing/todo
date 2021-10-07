051ec3ee143d3f5224d963d4c0502b9aa2b3ec32
David Taing <adavidtaing@gmail.com>
2021-10-07 23:48:54 +1100

    test: moved and refactored test

--- src/pages/api/users/login/index.test.ts
+++ src/pages/api/users/login/index.test.ts
@@ -3,17 +3,19 @@ import { createMocks } from "node-mocks-http";
 
 describe("/api/users/login", () => {
   describe("Root Handler", () => {
-    const { req, res } = createMocks();
- 
-    beforeAll(() => handler(req, res));
+    describe("Method Not Specified", () => {
+      const { req, res } = createMocks();
+  
+      beforeAll(() => handler(req, res));
 
-    test("405 Response Status Code", () => {
-      expect(res._getStatusCode()).toBe(405);
-    });
+      test("405 Response Status Code", () => {
+        expect(res._getStatusCode()).toBe(405);
+      });
 
-    test("Responds with JSON Message: 'Method Not Allowed'", () => {
-      const { message: actualMessage } = res._getJSONData();
-      expect(actualMessage).toBe("Method Not Allowed");
+      test("Responds with JSON Message: 'Method Not Allowed'", () => {
+        const { message: actualMessage } = res._getJSONData();
+        expect(actualMessage).toBe("Method Not Allowed");
+      });
     });
   });
 })
