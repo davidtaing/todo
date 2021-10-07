0db1680186ceaf05f4b9f38acc4c13cc30b04be6
David Taing <adavidtaing@gmail.com>
2021-10-07 23:44:51 +1100

    test: add red tests
    Checking for 405 status code response
    & "Method Not Allowed" JSON message

---
+++ src/pages/api/users/login/index.test.ts
@@ -0,0 +1,19 @@
+import handler from "./";
+import { createMocks } from "node-mocks-http";
+
+describe("/api/users/login", () => {
+  describe("Root Handler", () => {
+    const { req, res } = createMocks();
+ 
+    beforeAll(() => handler(req, res));
+
+    test("405 Response Status Code", () => {
+      expect(res._getStatusCode()).toBe(405);
+    });
+
+    test("Responds with JSON Message: 'Method Not Allowed'", () => {
+      const { message: actualMessage } = res._getJSONData();
+      expect(actualMessage).toBe("Method Not Allowed");
+    });
+  });
+})
