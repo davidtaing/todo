86aa3a27fa6b987e009c8ec7011e01be7fc53396
David Taing <adavidtaing@gmail.com>
2021-10-07 16:12:26 +1100

    Feat: api/todos stub handler

---
+++ src/pages/api/todos/index.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(500).json({ message: "Not implemented" });
+}
