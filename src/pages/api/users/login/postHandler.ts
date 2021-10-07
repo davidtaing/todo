0a36a4734bd765b998506a033b4a760fdb90940a
David Taing <adavidtaing@gmail.com>
2021-10-08 00:56:07 +1100

    style: add handler comment

--- src/pages/api/users/login/postHandler.ts
+++ src/pages/api/users/login/postHandler.ts
@@ -1,5 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+/**
+ * POST api/users/login
+ * @param req
+ * @param res 
+ */
 export function postHandler(req: NextApiRequest, res: NextApiResponse) {
   console.log("Called actual post handler");
   res.status(501).json({ message: "Not Implemented" });
