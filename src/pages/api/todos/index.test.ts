8355d20f7c8ef23e69f1e216f2ac93b3a4e81542
David Taing <adavidtaing@gmail.com>
2021-10-07 16:37:53 +1100

    Test: add failing test

---
+++ src/pages/api/todos/index.test.ts
@@ -0,0 +1,22 @@
+import handler from "./";
+import { createMocks } from "node-mocks-http";
+
+describe("api/todos", () => {
+  describe("GET api/todos", () => {
+    const { req, res } = createMocks({
+      method: "GET",
+      body: {
+        uid: "gKl5yZeYDXzQ6mYzRZsIfq7JqNta",
+      }
+    });
+
+    beforeAll(() => handler(req, res));
+
+    test("Returns An Array of Todos", () => {
+      const result = res._getJSONData();
+      console.log(result)
+
+      expect(Array.isArray(result)).toBeTruthy();
+    });
+  });
+});
