4cb55cc2533ed996d836f31034e3e1793e844b74
David Taing <adavidtaing@gmail.com>
2021-10-08 00:15:07 +1100

    feat: add stub postHandler in prep for red test

--- src/pages/api/users/login/index.ts
+++ src/pages/api/users/login/index.ts
@@ -3,3 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse): void {
   res.status(405).json({ message: "Method Not Allowed" });
 };
+
+export function postHandler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(501).json({ message: "Not Implemented" });
+}
