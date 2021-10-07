6a493e144f9392702fe6dd306892f260a8245119
David Taing <adavidtaing@gmail.com>
2021-10-08 00:15:24 +1100

    test: add red test "is postHandler called?"

--- src/pages/api/users/login/index.test.ts
+++ src/pages/api/users/login/index.test.ts
@@ -1,8 +1,31 @@
-import handler from "./";
+import handler, { postHandler } from "./";
 import { createMocks } from "node-mocks-http";
+
+
+jest.mock("./", () => {
+  const originalModule = jest.requireActual("./");
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    postHandler: jest.fn(),
+  }
+});
 
 describe("/api/users/login", () => {
   describe("Root Handler", () => {
+    describe("POST /api/users/login", () => {
+      const { req, res } = createMocks({
+        method: "POST",
+      });
+  
+      beforeAll(() => handler(req, res));
+
+      test("is postHandler called?", () => {
+        expect(postHandler).toHaveBeenCalled();
+      });
+    });
+
     describe("Method Not Specified", () => {
       const { req, res } = createMocks();
   
