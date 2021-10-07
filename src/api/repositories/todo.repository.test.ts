58ba9d39d9f60707505f757e0ed7a6fc35753223
David Taing <adavidtaing@gmail.com>
2021-10-07 15:51:06 +1100

    Refactor: test

--- src/api/repositories/todo.repository.test.ts
+++ src/api/repositories/todo.repository.test.ts
@@ -1,16 +1,10 @@
 import TodoRepository from "./todo.repository";
 
-describe("Test Repository", () => {
-  describe("Returns Array of Todos", () => {
-    const todoRepository = new TodoRepository();
-    let result;
-    beforeAll(async () => {
-      result = await todoRepository.getAll("gKl5yZeYDXzQ6mYzRZsIfq7JqNta");
-      console.log(result);
-    })
+describe("TodoRepository", () => {
+  const todoRepository = new TodoRepository();
 
-    test("Returns Todos Array", () => {
-      expect(Array.isArray(result)).toBeTruthy();
-    })
+  test("Returns Todos Array", async () => {
+    const result = await todoRepository.getAll("gKl5yZeYDXzQ6mYzRZsIfq7JqNta");
+    expect(Array.isArray(result)).toBeTruthy();
   });
-})
+});
