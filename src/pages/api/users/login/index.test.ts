261411c8e901d583460f849381b979ed2a88e394
David Taing <adavidtaing@gmail.com>
2021-10-08 00:58:50 +1100

    test: add delete /api/users/login test

--- src/pages/api/users/login/index.test.ts
+++ src/pages/api/users/login/index.test.ts
@@ -6,7 +6,7 @@ jest.mock("./postHandler");
 
 describe("/api/users/login", () => {
   describe("Root Handler", () => {
-    describe("POST /api/users/login", () => {
+    describe("POST (Allowed)", () => {
       const { req, res } = createMocks({
         method: "POST",
       });
@@ -18,7 +18,24 @@ describe("/api/users/login", () => {
       });
     });
 
-    describe("Method Not Specified", () => {
+    describe("DELETE (Not Allowed)", () => {
+      const { req, res } = createMocks({
+        method: "DELETE",
+      });
+  
+      beforeAll(() => handler(req, res));
+
+      test("405 Response Status Code", () => {
+        expect(res._getStatusCode()).toBe(405);
+      });
+
+      test("Responds with JSON Message: 'Method Not Allowed'", () => {
+        const { message: actualMessage } = res._getJSONData();
+        expect(actualMessage).toBe("Method Not Allowed");
+      });
+    });
+
+    describe("No HTTP METHOD", () => {
       const { req, res } = createMocks();
   
       beforeAll(() => handler(req, res));
