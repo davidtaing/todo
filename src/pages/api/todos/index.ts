39adb1eade5073ac4ace8a3027591f99bd87e205
David Taing <adavidtaing@gmail.com>
2021-10-07 19:03:12 +1100

    Refactor: adjust implementation to fail test

--- src/pages/api/todos/index.ts
+++ src/pages/api/todos/index.ts
@@ -2,6 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import TodoRepository from "../../../api/repositories/todo.repository";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const method;
+
+  switch (method) {
+    default:
+      return res.status(500).json({ message: "Not implemented" });
+  }
   return getHandler(req, res);
   res.status(500).json({ message: "Not implemented" });
 }
