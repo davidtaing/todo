316db716cc71734c9d2431a00a4aaf31007e7049
David Taing <adavidtaing@gmail.com>
2021-10-08 15:05:40 +1100

    refactor: wrapped process.env to config util
    In preparation to test how auth.ts responds to NODE_ENV
    and if it decides to connect to the emulators

--- src/api/firebase/auth.ts
+++ src/api/firebase/auth.ts
@@ -1,10 +1,11 @@
 import { getAuth, connectAuthEmulator } from "@firebase/auth";
 import { firebaseApp } from "./";
+import config from "../../utils/config";
 
 export const auth = getAuth(firebaseApp);
 
-if (process.env.NODE_ENV !== "production") {
-  const { FIREBASE_EMULATOR_HOST: host, FIREBASE_AUTH_EMULATOR_PORT: port } = process.env;
+if (config.NODE_ENV !== "production") {
+  const { FIREBASE_EMULATOR_HOST: host, FIREBASE_AUTH_EMULATOR_PORT: port } = config;
   const emulatorUrl = `http://${host}:${port}/`;
   connectAuthEmulator(auth, emulatorUrl);
 }
---
+++ src/utils/config.ts
@@ -0,0 +1,6 @@
+/**
+ * Acts as a wrapper for process.env and allows us to mock NODE_ENV
+ */
+const config = process.env;
+
+export default config;
