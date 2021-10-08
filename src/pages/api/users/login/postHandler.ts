ef676117fb8e61eced9321897cc16472cd2026c8
David Taing <adavidtaing@gmail.com>
2021-10-08 21:53:03 +1100

    style: added comment to handler

--- src/pages/api/users/login/postHandler.ts
+++ src/pages/api/users/login/postHandler.ts
@@ -2,8 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 /**
  * POST api/users/login
- * @param req
- * @param res 
+ * @body email: string
+ * @body password: string
+ * @responses 200 OK - User Data { name: string, todos: []}
+ * @responses 400 Bad Request
+ * @responses 401 Unauthorized - "Invalid Email or Password"
+ * @responses 500 Internal Server Error
  */
 export default function postHandler(req: NextApiRequest, res: NextApiResponse) {
   console.log("Called actual post handler");
