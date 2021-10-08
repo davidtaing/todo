ebce20f408af10acc26c14a3dc806a3f3dd532a6
David Taing <adavidtaing@gmail.com>
2021-10-09 02:22:27 +1100

    style: cleaned up code

--- src/pages/api/users/register/postHandler.ts
+++ src/pages/api/users/register/postHandler.ts
@@ -29,7 +29,7 @@ export default async function postHandler(
         .json({ message: "Password and Confirm Password do not match." });
     }
 
-    const userCredential: any = await createUserWithEmailAndPassword(
+    await createUserWithEmailAndPassword(
       auth,
       email,
       password
