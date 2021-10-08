7f49abc4b720d37f2ced36725842547d38256cf4
David Taing <adavidtaing@gmail.com>
2021-10-09 00:27:21 +1100

    fix: broken test due to previous commit (#42cacd3)
    "Connects to Auth Emulator" test failed due to property
    being undefined after mocking

--- src/api/firebase/auth.ts
+++ src/api/firebase/auth.ts
@@ -4,11 +4,13 @@ import { firebaseApp } from "./";
 
 export const auth = getAuth(firebaseApp);
 
-// check _canInitEmulator to stop attempting to reconnect emulators
-if (config.NODE_ENV !== "production" && (auth as any)._canInitEmulator) {
-  const { host, authPort } = config.firebaseEmulator;
-  const emulatorUrl = `http://${host}:${authPort}/`;
-  connectAuthEmulator(auth, emulatorUrl);
+if (config.NODE_ENV !== "production") {
+  // check _canInitEmulator to stop attempting to reconnect emulators
+  if ((auth as any)?._canInitEmulator ?? true) {
+    const { host, authPort } = config.firebaseEmulator;
+    const emulatorUrl = `http://${host}:${authPort}/`;
+    connectAuthEmulator(auth, emulatorUrl);
+  }
 }
 
 export { signInWithEmailAndPassword, createUserWithEmailAndPassword };
