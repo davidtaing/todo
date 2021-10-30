52b5cfeef1dfb72296c024bd2cc7e7ff17431704
David Taing <adavidtaing@gmail.com>
2021-10-30 21:35:43 +1100

    styling: styled body to fill the screen

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -5,8 +5,8 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
     width: 100vw;
+    height: 100vh;
     background-color: ${theme.secondary};
   }
 
